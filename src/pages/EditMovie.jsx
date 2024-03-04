a813fa79b1682b596fd4d856c8341a444889d708
Sujan STC <sujandstc@gmail.com>
2024-03-04 16:28:49 +0545

    Changes

--- src/pages/EditMovie.jsx
+++ src/pages/EditMovie.jsx
@@ -1,15 +1,39 @@
 import axios from "axios";
 import Title from "../components/title";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const EditMovie = () => {
+  const navigate = useNavigate();
+
+  const { movie_id } = useParams();
+
   const movie_name_ref = useRef();
   const rating_ref = useRef();
   const info_ref = useRef();
   const image_ref = useRef();
 
+  useEffect(() => {
+    getSingleMovies(movie_id);
+  }, []);
+
+  const getSingleMovies = async (movie_id) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/movie/${movie_id}`
+      );
+      movie_name_ref.current.value = response.data.moviesData.name ?? "";
+      rating_ref.current.value = response.data.moviesData.rating ?? "";
+      info_ref.current.value = response.data.moviesData.info ?? "";
+      image_ref.current.value = response.data.moviesData.image ?? "";
+    } catch (e) {
+      alert("Err");
+    }
+  };
+
   const editMovie = async () => {
     const dataToSend = {
+      movie_id: movie_id,
       name: movie_name_ref.current.value,
       rating: rating_ref.current.value,
       info: info_ref.current.value,
@@ -18,6 +42,7 @@ const EditMovie = () => {
 
     try {
       await axios.patch(`http://localhost:4000/movies`, dataToSend);
+      navigate(-1);
     } catch (e) {
       console.log(e);
       alert("Cannot add new movie!");
@@ -31,7 +56,7 @@ const EditMovie = () => {
         className="p-3"
         onSubmit={(e) => {
           e.preventDefault();
-          EditMovie();
+          editMovie();
         }}
       >
         Movie name:
